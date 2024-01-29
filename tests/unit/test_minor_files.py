9eed399cac1f3ae5d68946f4311a685b48d01adb
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-01-29 15:24:20 -0500

    Fix tests

--- tests/unit/test_minor_files.py
+++ tests/unit/test_minor_files.py
@@ -14,22 +14,7 @@ def test_write_sleeplog(tmp_path: pathlib.Path, file_manager: dict[str, str]) ->
         lines = f.readlines()
 
     assert lines[0] == "ID,onset_N1,wakeup_N1\n"
-    assert (
-        lines[1] == "identifier,1993-08-26 12:00:00+00:00,1993-08-26 13:00:00+00:00\n"
-    )
-
-
-def test_write_vector(tmp_path: pathlib.Path) -> None:
-    """Test the write_vector function."""
-    sample_vector = [1, 2, 3, 4, 5]
-    expected = "1,2,3,4,5\n"
-    vector_path = tmp_path / "test_vector.csv"
-
-    ggir_files.write_vector(str(vector_path), sample_vector)
-    with open(vector_path, encoding="utf-8") as file_buffer:
-        actual = file_buffer.read()
-
-    assert actual == expected
+    assert lines[1] == "subject,1993-08-26 12:00:00+00:00,1993-08-26 13:00:00+00:00\n"
 
 
 def test_flatten() -> None:
